f08b38cce0509eb13c01403b290e46b1d6b91ca0
Nicole <nicoleshasha@hotmail.co.uk>
2017-01-31 14:40:45 +0000

    Add fail message

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,12 +3,13 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
-  def release_bike
-    Bike.new
-  end
-
   def dock(bike)
     @bike = bike
+  end
+
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
 end
